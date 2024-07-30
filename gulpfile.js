f78deeaa5877419ef4302b9ae0ca56e3509d07ce
ArgeisGutierrez <argenis.v.gtz@gmail.com>
2024-07-30 16:04:25 -0600

    feat(full)Add Header Inicio
    Se agrego el herader de la pagina de inicio junto con sus estilos.
    Se agregaron las imagenes del proyecto.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
 import { dest, src, watch } from "gulp";
+//con ESM se tiene que jugar con los nombres
 import sass from "gulp-sass";
 import * as sassP from 'sass';
 const sassCompiler = sass(sassP);
+
+//Postcss y Autoprefixer
 import postcss from "gulp-postcss";
 import autoprefixer from "autoprefixer";
+
+//Minificar Imagenes
+import imagemin, { optipng } from "gulp-imagemin";
+// import gulpAvif from 'gulp-avif';
+import gulpAvif from 'gulp-avif';
 
 export function css(done) {
   src('src/scss/app.scss')
@@ -13,6 +21,21 @@ export function css(done) {
   done();
 }
 
+// export function imgs(done) {
+//   src('src/img/**/*')
+//     // .pipe(imagemin())
+//     .pipe(dest('build/img'))
+//   done();
+// }
+
+// export function imgsAvif(done) {
+//   src('src/img/**/*.jpg')
+//     .pipe(gulpAvif())
+//     .pipe(dest('build/img'))
+//   done();
+// }
+
 export function dev() {
   watch('src/scss/**/*.scss', css);
+  // watch('src/img/**/*', imgs);
 }
